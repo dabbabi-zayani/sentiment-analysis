737b65a53add2ca42e821c240c086cb381a4c9bd
Sanjeev KC <sanjeevkumar.chitters@gmail.com>
2015-02-24 15:27:54 -0700

    Update naivebayes.py

--- naivebayes.py
+++ naivebayes.py
@@ -6,6 +6,14 @@ from collections import Counter
 import cPickle as pickle
 import textblob
 
+#global variables
+
+pos_total_words = 0
+neg_total_words = 0
+neutral_total_words = 0
+total_unique_words = 0
+accuracy = 0
+
 #updates tweets[] by filtering pos and neg tweets together passing
 #through stopwords.txt
 
@@ -19,7 +27,6 @@ def filterTweets(trData):
         trainData.append(([t for t in txt if t not in stopWords],label))
     #print "trainData","\n",trainData,"\n" #[(['love', 'car'], 'positive')]
     return trainData
-
 # noinspection PyUnreachableCode
 def _get_features(f_tweets):
 
@@ -55,63 +62,120 @@ def prior_prob(f_tweets):
 
     count = Counter(elem[1] for elem in f_tweets)
     total_classes = count['positive'] + count['negative'] + count['neutral']
-
+    #print total_classes
     pos_prob = float(count['positive'])/total_classes
     neg_prob = float(count['negative'])/total_classes
     neutral_prob = float(count['neutral'])/total_classes
 
     #print {'p(positive)':pos_prob,'p(negative)':neg_prob,'p(neutral)':neutral_prob},"\n"
-    return {'p(positive)':pos_prob,'p(negative)':neg_prob,'p(neutral)':neutral_prob}
+    return {'class_positive_prob':pos_prob,'class_negative_prob':neg_prob,'class_neutral_prob':neutral_prob}
 
 def posterior_prob(g_features):
 
-    training_sets = []
-    pos_total_words = neg_total_words = neutral_total_words = 0
+    global pos_total_words,neg_total_words,neutral_total_words,total_unique_words
+
+    pos_sets={}
+    neg_sets={}
+    neu_sets={}
+
     #global _feature_vector,_trained_Data
     #repeated values included
-    for each_feature in g_features:
-        if each_feature[1] == 'positive':
-            pos_total_words += sum(each_feature[0].values())
-        elif each_feature[1] == 'negative':
-            neg_total_words += sum(each_feature[0].values())
+
+    for each_feature,label in g_features:
+        #print each_feature,label
+        if label == 'positive':
+            pos_total_words += sum(each_feature.values())
+        elif label == 'negative':
+            neg_total_words += sum(each_feature.values())
         else:
-            neutral_total_words += sum(each_feature[0].values())
+            neutral_total_words += sum(each_feature.values())
     total_unique_words = len(g_features)
-    #print pos_total_words,neg_total_words,total_unique_words
+    #print pos_total_words,neg_total_words,neutral_total_words,total_unique_words
 
     for each_feature,label in g_features:
         #for key,value in each_feature.iteritems():
+        #print each_feature,label
         if label == 'positive':
-            training_sets.append([
-                [key for key,value in each_feature.iteritems()][0],label,
-                float(sum(each_feature.values())+1)/
-                (pos_total_words + total_unique_words)])
+            pos_sets[[key for key,value in each_feature.iteritems()][0]] =\
+                float(sum(each_feature.values())+1)/\
+                (pos_total_words + total_unique_words)
         elif label == 'negative':
-            training_sets.append([
-                [key for key,value in each_feature.iteritems()][0],label,
-                float(sum(each_feature.values())+1)/
-                (neg_total_words + total_unique_words)])
-        else:
-            training_sets.append([
-                [key for key,value in each_feature.iteritems()][0],label,
-                float(sum(each_feature.values())+1)/
-                (neutral_total_words + total_unique_words)])
-
-    #print "training_sets","\n", training_sets
-    return training_sets
+            neg_sets[[key for key,value in each_feature.iteritems()][0]] =\
+                float(sum(each_feature.values())+1)/\
+                (neg_total_words + total_unique_words)
+        elif label == 'neutral':
+            neu_sets[[key for key,value in each_feature.iteritems()][0]] =\
+                float(sum(each_feature.values())+1)/\
+                (neutral_total_words + total_unique_words)
+    #print pos_sets,neg_sets,neu_sets
+    return pos_sets,neg_sets,neu_sets
 
 def trainNBClassifier(trData):
 
     tweets = filterTweets(trData)
-    with open('TRAIN_SETS.PICKLE', 'wb') as classifier:
-        pickle.dump(posterior_prob(_get_features(tweets)),classifier)
-        pickle.dump(prior_prob(tweets), classifier)
+    with open('TRAIN_SETS.PICKLE', 'wb') as classifier_file:
+        pickle.dump(posterior_prob(_get_features(tweets)),classifier_file)
+        pickle.dump(prior_prob(tweets), classifier_file)
 
-def testNBClassifier():
-    #tweets = filterTweets(testData)
+def predict_label_aux(classifier,tweet,class_probabilities):
+
+    global accuracy,pos_total_words,neg_total_words,neutral_total_words,total_unique_words
+    pos_prob = 0
+    neg_prob = 0
+    neu_prob = 0
+
+    text = tweet[0]
+    label = tweet[1]
+
+    for t in text:
+
+        if t in classifier[0]:
+            pos_prob += classifier[0][t]
+        else:
+            pos_prob += float(1)/(pos_total_words + total_unique_words)
+            classifier[0][t] = float(1)/(pos_total_words + total_unique_words)
+
+        if t in classifier[1]:
+            neg_prob += classifier[1][t]
+        else:
+            neg_prob += float(1)/(neg_total_words + total_unique_words)
+            classifier[1][t] = float(1)/(neg_total_words + total_unique_words)
+
+        if t in classifier[2]:
+            neu_prob += classifier[2][t]
+        else:
+            neu_prob += float(1)/(neutral_total_words + total_unique_words)
+            classifier[2][t] = float(1)/(neutral_total_words + total_unique_words)
+
+    pos_prob *= class_probabilities['class_positive_prob']
+    neg_prob *= class_probabilities['class_negative_prob']
+    neu_prob *= class_probabilities['class_neutral_prob']
+
+    max_prob = max(pos_prob,neg_prob,neu_prob)
+    if max_prob == pos_prob:
+        predicted_label = 'positive'
+    elif max_prob == neg_prob:
+        predicted_label = 'negative'
+    else:
+        predicted_label = 'neutral'
+
+    if predicted_label == label:
+        accuracy+=1
+
+    return predicted_label
+
+def predict_label(classifier,tweets,class_probabilities):
+    for tweet in tweets:
+        print tweet, " is ",predict_label_aux(classifier,tweet,class_probabilities)
+
+def testNBClassifier(testData):
     with open('TRAIN_SETS.PICKLE', 'rb') as f:
         classifier = pickle.load(f)
-    print classifier
+        prior_probs = pickle.load(f)
+    testData_features = filterTweets(testData)
+    predict_label(classifier,testData_features,prior_probs)
+    print "Accuracy is ", (float(accuracy)/len(testData))*100,"%"
+    print "\n"
 
 pos_tweets = [('I love this car', 'positive'),
               ('This is amazing', 'positive')]
@@ -124,7 +188,13 @@ neu_tweets = [('I bought this car', 'neutral'),
               ('This is the view', 'neutral'),
               ('I woke up stupid morning', 'neutral')]
 
-td = pos_tweets + neg_tweets + neu_tweets
+tr_data = pos_tweets + neg_tweets + neu_tweets
 
-trainNBClassifier(td)
-testNBClassifier()
+test_data = [('I feel happy this morning','positive'),
+             ('Larry is my friend','positive'),
+             ('I do not like that man','negative'),
+             ('My house is not great','negative'),
+             ('Your song is annoying','negative')]
+
+trainNBClassifier(tr_data)
+testNBClassifier(test_data)
